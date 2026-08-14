e94191e14528e2ae12662c69019a32287c64a049
ivucicev <ivucicev@gmail.com>
2026-08-14 17:20:55 +0200

    fix: use named imports for TextStyle and Color (no default export in tiptap v3)

--- components/RichTextEditor.tsx
+++ components/RichTextEditor.tsx
@@ -3,8 +3,8 @@ import StarterKit from "@tiptap/starter-kit";
 import Image from "@tiptap/extension-image";
 import Link from "@tiptap/extension-link";
 import Underline from "@tiptap/extension-underline";
-import TextStyle from "@tiptap/extension-text-style";
-import Color from "@tiptap/extension-color";
+import { TextStyle } from "@tiptap/extension-text-style";
+import { Color } from "@tiptap/extension-color";
 import { useEffect, useRef } from "react";
 import {
   RiBold, RiItalic, RiUnderline, RiLink, RiImageLine,
